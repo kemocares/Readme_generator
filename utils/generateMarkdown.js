d46fe3178f082a733a1c6413c7dfcd663b34bafc
Oluwakemi Folorunsho <folorunshooluwakemic@gmail.com>
2023-01-29 01:33:00 +0000

    Completed generate Markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,51 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description 
+
+${data.description}
+
+
+## Table of Contents 
+
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## <a id="installation">Installation</a>
+
+${data.installation}
+
+
+## <a id="usage">Usage</a>
+
+${data.usage}
+
+
+## <a id="license">License</a>
+
+Copyright (c) 2023 Oluwakemi Folorunsho. All rights reserved.
+Licensed under the [MIT](LICENSE.md) license.
+
+
+## <a id="contributing">Contributing</a>
+
+For information on how to contribute to this project,see our [code of conduct](CONTRIBUTING.md).
+
+
+## <a id="tests">Tests</a>
+
+${data.tests}
+
+
+## <a id="questions">Questions</a>
+
+${data.questions}
 `;
 }
 
